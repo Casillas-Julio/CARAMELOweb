c61be2f8c980e49b809c0648c09c6ce30bbca7de
Julio Casillas <julio1987sara@hotmail.com>
2015-08-05 21:13:01 -0500

    logo

--- TodoServicios/index.php
+++ TodoServicios/index.php
@@ -7,16 +7,17 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Todo Servicios</title>
     </head>
     <body>
 
         <header>
             <div>
-                
+                <a href="index.php"><img src="images/logo.png" alt="todoservicio.com logo"/></a>
             </div>      
         </header>
           
+        
         
         <footer>
             
