f79725730e5240ae5fd6c7abf2a3a291334848b6
ISC Oscar Z <ISC Oscar Z@BLANCAZAVALA>
2015-08-10 00:58:55 -0500

    ME PASÉ TODA LA NOCHE PASANDO ESTO !!!

--- TodoServicios/index.php
+++ TodoServicios/index.php
@@ -8,19 +8,25 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Todo Servicios</title>
+        <meta name="Description" content="Ofrece y recibe los mejores servicios al instante. ">
     </head>
+    
     <body>
 
         <header>
             <div>
-                <a href="index.php"><img src="images/logo.png" alt="todoservicio.com logo"/></a>
+                <p ALIGN=center>Los mejores servicios encuentralos al instante</p>
+                <a href="index.php"><img src="images/logo.png" width="111px" height="222px" alt="todoservicio.com logo"/></a>
             </div>      
         </header>
           
         
         
         <footer>
-            
+            <HR align="CENTER" size="2" width="1000" color="Red" noshade>
+            <p ALIGN=center> <i>Todos los derechos reservados</i> </p>
+            <p ALIGN=center>San Luis Potosí, S.L.P.</p>
+            <p ALIGN=center>Simplemente estoy probando el <b>footer</b> </p>
         </footer>
         
     </body>
