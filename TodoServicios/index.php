280019ae46ebad7e378c82255e51c7124673e96b
ISC Oscar Z <ISC Oscar Z@BLANCAZAVALA>
2015-08-31 21:08:04 -0500

    PLANNING!!!

--- TodoServicios/index.php
+++ TodoServicios/index.php
@@ -15,6 +15,15 @@ and open the template in the editor.
     <body>
 
         <header>
+            <table width="100%" border="1" bordercolor="#0000FF" cellspacing="10" cellpadding="10">
+                <tr>
+                    <td><a href="index.php"><img src="images/logo.png" width="222px" height="222px" alt="todoservicio.com logo"/></a></td>
+                    <td>ANUNCIA SERVICIO</td>
+                    <td>ENCUENTRA SERVICIO</td>
+                    <td>CONTACTANOS</td>
+                </tr>
+            </table>
+            
             <div>
                 <p ALIGN=center>Los mejores servicios encuentralos al instante</p>
                 <a href="index.php"><img src="images/logo.png" width="222px" height="222px" alt="todoservicio.com logo"/></a>
@@ -25,9 +34,36 @@ and open the template in the editor.
                 </div>
             </div>      
         </header>
+        
+        <nav>
+            <ul>
+                <li><a href="cursohtml.html">encuentra un servicio</a></li>
+                <li><a href="cursocss.html">Anuncia un servicio</a></li>
+                <li><a href="cursojavascript.html">contactanos</a></li>
+            </ul>
+        </nav>
 
-
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         <footer>
             <HR align="CENTER" size="2" width="1000" color="Red" noshade>
             <p ALIGN=center> <i>Todos los derechos reservados</i> </p>
